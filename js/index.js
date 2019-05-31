2ec724c8b5b66f2b966eaf33236a4a6fb710f7f0
hao <2991944127@qq.com>
2019-05-31 18:01:16 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 
-//页面效果
 var PageAnimate = (function () {
-    //碎片跟随鼠标动
     $('.logo618').each(function () {
         $(this).data('left', parseInt($(this).css('left').match(/^-?\d*/)[0]));
         $(this).data('top', parseInt($(this).css('top').match(/^-?\d*/)[0]));
@@ -17,7 +15,6 @@ var PageAnimate = (function () {
     };
 })();
 
-// logo进场缩小
 setTimeout(function () {
     $('.logo618').animate({
         'top': '200px',
@@ -30,19 +27,17 @@ setTimeout(function () {
 window.addEventListener("scroll", function () {
     if (document.documentElement.scrollTop > 1000) {
         $('.ce-618').fadeIn()
-    }else{
+    } else {
         $('.ce-618').fadeOut()
     }
 
 });
 
-// 四个牌子
 nav168($('.nav618-1'), '367px', '347px', '378px', '398px', 100)
 nav168($('.nav618-2'), '318px', '298px', '645px', '665px', 100)
 nav168($('.nav618-3'), '340px', '320px', '990px', '990px', 100)
 nav168($('.nav618-4'), '380px', '360px', '1265px', '1265px', 100)
 
-//四个牌子函数
 function nav168(e, width, widthed, left, lefted, speed) {
     e.hover(function () {
         $(this).find('img').eq(1).css('display', 'none')
@@ -61,27 +56,23 @@ function nav168(e, width, widthed, left, lefted, speed) {
     })
 }
 
- 
-     
-    // 福利
-    $( '.register-menu-btn').click(function(){
-         $('.register-dialog-box li').eq($(this).index()).addClass('w-block').siblings().removeClass('w-block')
-    })
-// 关闭福利弹窗
-    $('.register-close').click(function(){
-        $('.register-dialog').fadeOut()
-    })
-//右侧导航
-$('.ce-menu a').click(function(){
-        $('.ce-menu a').removeClass('w-active')
-        $(this).addClass('w-active')
-    })
 
 
-//轮播index
+$('.register-menu-btn').click(function () {
+    $('.register-dialog-box li').eq($(this).index()).addClass('w-block').siblings().removeClass('w-block')
+})
+$('.register-close').click(function () {
+    $('.register-dialog').fadeOut()
+    $('.register-dialog-box li').removeClass('w-block')
+})
+$('.ce-menu a').click(function () {
+    $('.ce-menu a').removeClass('w-active')
+    $(this).addClass('w-active')
+})
+
+
 var chongzhi = 0;
 
-// 充值右边
 $('#chongzhi-r').click(function () {
     chongzhi++;
     if (chongzhi < 0) { chongzhi = 0 }
@@ -93,7 +84,6 @@ $('#chongzhi-r').click(function () {
         'left': -(chongzhi * 223) + 'px'
     })
 })
-// 充值左边
 $('#chongzhi-l').click(function () {
     chongzhi--;
     if (chongzhi < 0) { $('.chongzhi-list-box ul').css('left', -$('.chongzhi-list-box ul').width() / 2 + 'px'); chongzhi = $('.chongzhi-list-box li').length / 2 - 1; }
@@ -101,9 +91,7 @@ $('#chongzhi-l').click(function () {
         'left': -(chongzhi * 223) + 'px'
     })
 })
-// 定时器
 var chongzhi_timer = setInterval(chongzhi_time, 2000)
-// 定时器函数
 function chongzhi_time() {
     chongzhi++;
     if (chongzhi < 0) { chongzhi = 0 }
@@ -115,22 +103,21 @@ function chongzhi_time() {
         'left': -(chongzhi * 223) + 'px'
     })
 }
-// 清除定时器
 $('.chongzhi-list').mouseenter(function () {
     clearInterval(chongzhi_timer)
 })
 $('.chongzhi-list').mouseleave(function () {
-    // 定时器
+
     chongzhi_timer = setInterval(chongzhi_time, 2000)
 })
 
 
 
 
-// 渲粉
+
 
 var xuan = 0;
-// 渲粉右边
+
 $('#xuan-r').click(function () {
     xuan++;
     if (xuan < 0) { xuan = 0 }
@@ -143,7 +130,7 @@ $('#xuan-r').click(function () {
     })
 })
 
-// 渲粉左边
+
 $('#xuan-l').click(function () {
     xuan--;
     if (xuan < 0) { $('.xuan-conent-box ul').css('left', -$('.xuan-conent-box ul').width() / 2 + 'px'); xuan = $('.xuan-conent-box li').length / 2 - 1; }
@@ -152,9 +139,9 @@ $('#xuan-l').click(function () {
     })
 })
 
-// 定时器
+
 var xuan_timer = setInterval(xuan_time, 2000)
-// 定时器函数
+
 function xuan_time() {
     xuan++;
     if (xuan < 0) { xuan = 0 }
@@ -166,19 +153,18 @@ function xuan_time() {
         'left': -(xuan * 249) + 'px'
     })
 }
-// 清除定时器
+
 $('.xuan-conent').mouseenter(function () {
     clearInterval(xuan_timer)
 })
 $('.xuan-conent').mouseleave(function () {
-    // 定时器
+
     xuan_timer = setInterval(xuan_time, 2000)
 })
 
 
-// 提升
 var tisheng = 0;
-// 提升右边
+
 $('#tisheng-r').click(function () {
     tisheng++;
     if (tisheng < 0) { tisheng = 0 }
@@ -190,7 +176,7 @@ $('#tisheng-r').click(function () {
         'left': -(tisheng * 527) + 'px'
     })
 })
-// 渲粉左边
+
 $('#tisheng-l').click(function () {
     tisheng--;
     if (tisheng < 0) { $('.tisheng-list-box ul').css('left', -$('.tisheng-list-box ul').width() / 2 + 'px'); tisheng = $('.tisheng-list-box li').length / 2 - 1; }
@@ -199,9 +185,9 @@ $('#tisheng-l').click(function () {
     })
 })
 
-// 定时器
+
 var tisheng_timer = setInterval(tisheng_time, 2000)
-// 定时器函数
+
 function tisheng_time() {
     tisheng++;
     if (tisheng < 0) { tisheng = 0 }
@@ -213,12 +199,12 @@ function tisheng_time() {
         'left': -(tisheng * 527) + 'px'
     })
 }
-// 清除定时器
+
 $('.tisheng-list').mouseenter(function () {
     clearInterval(tisheng_timer)
 })
 $('.tisheng-list').mouseleave(function () {
-    // 定时器
+
     tisheng_timer = setInterval(tisheng_time, 2000)
 })
 
@@ -228,7 +214,44 @@ $('.tisheng-list').mouseleave(function () {
 
 
 
+$(function () {
+    var payMethod = true 
+    // 点击充值
+    $('.chongzhi').bind('click', function () {
+        $('.ars-pay').show()
+        // $('.ars-pay-tip').html('<span>实际支付:</span>' + $(this).siblings().text() + '.00' + '<small>元</small>')
+    })
 
+    // 选择支付方式
+    $('.ars-pay-item').click(function () {
+        console.log('sdfsdf')
+        $(this).addClass('ars-active').siblings().removeClass('ars-active')
+        payMethod = !payMethod
+        if (payMethod == true) {
+            $('#zhifubao').hide()
+            $('#weixin').show()
+        } else {
+            $('#zhifubao').show()
+            $('#weixin').hide()
+        }
+    })
+
+    //关闭充值
+    $('.ars-pay-close').click(function () {
+        $('.ars-pay').hide()
+    })
+
+    // 立即支付（选择微信）
+    $('#weixin').click(function () {
+        $('.ars-pay').hide()
+        $('.ars-weixin').show()
+    })
+    // 关闭微信二维码弹出框
+    $('.ars-weixin-close').click(function () {
+        $('.ars-weixin').hide()
+    })
+
+})
 
 
 
